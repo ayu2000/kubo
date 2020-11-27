1124f16f26ae395e12393d0aeb14c14375aece1d
ayu2000 <69434179+ayu2000@users.noreply.github.com>
2020-11-27 14:18:20 +0900

    Update setting.js

--- portfolio/js/setting.js
+++ portfolio/js/setting.js
@@ -51,3 +51,19 @@ $(function(){
         return false;
     });
 });
+
+var topBtn = $('#page-top');    
+    topBtn.hide();
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+    topBtn.fadeIn();
+    } else {
+    topBtn.fadeOut();
+    }
+});
+topBtn.click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 500);
+    return false;
+});
